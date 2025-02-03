2448557f3f24b79752a7505e49c3882ab8afd5c4
eliguy40 <elijahjc000@gmail.com>
2025-02-03 12:44:44 -0500

    exercises-week-19

---
+++ excercises/exercises-week-19.py
@@ -0,0 +1,6 @@
+
+fruits = ["Apples", "Oranges", "Grapefruit"]
+
+vegetables = ["Broccoli", "Cabbage", "Lettuce", "Bell Peppers"]
+
+shopping_list = [["Milk", "Soda", "O.J."], ["Honey Bunches", "Granola", "Life"]]
