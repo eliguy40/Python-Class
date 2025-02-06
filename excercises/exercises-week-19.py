6097f902fb080eed817e68059634d754b864ecbb
eliguy40 <elijahjc000@gmail.com>
2025-02-05 23:52:23 -0500

    week 19 exercises

--- excercises/exercises-week-19.py
+++ excercises/exercises-week-19.py
@@ -4,3 +4,12 @@ fruits = ["Apples", "Oranges", "Grapefruit"]
 vegetables = ["Broccoli", "Cabbage", "Lettuce", "Bell Peppers"]
 
 shopping_list = [["Milk", "Soda", "O.J."], ["Honey Bunches", "Granola", "Life"]]
+
+shopping_list.append(vegetables)
+
+shopping_list.append(fruits)
+
+# print(shopping_list)
+
+print(shopping_list[1])
+
